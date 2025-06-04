7dc90cdf87ce566358e9a0c270163ffa495d088a
GabrielNatividadeBS <gabriel.natividade2011@gmail.com>
2025-06-03 22:17:40 -0300

    Atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        Televisao tv = new Televisao(80, "Philips", 220, 30);
+        Televisao tv = new Televisao(80, "Philip", 220, 30);
 
 
         tv.status();
